0b2f03327fc9df6fdb70e4a1dff5371c84e5a1c2
AlberttVs <158062200+AlberttVs@users.noreply.github.com>
2024-12-01 01:49:36 +0100

    Delete php directory

--- php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php //la funcion que tiene esta pagina es cerrar la sesion que hay iniciada tras poner las credenciales correctamente en el login.
-
-session_start();
-
-session_destroy();
-
-header("Location: ../login.html"); //nos redirige al index.php
-
-?>
