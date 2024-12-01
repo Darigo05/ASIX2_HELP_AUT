697cff8d02dab41b7d450a3261b5d9ee236e1149
AlberttVs <158062200+AlberttVs@users.noreply.github.com>
2024-12-01 01:49:14 +0100

    Delete js directory

--- js/blog.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("load-more-btn").addEventListener("click", function() {
-    // Mostrar las tarjetas adicionales
-    var additionalBlogs = document.getElementById("additional-blogs");
-    additionalBlogs.style.display = "block";
-    
-    // Ocultar el botón "Explora más blogs" después de hacer clic
-    this.style.display = "none";
-});
